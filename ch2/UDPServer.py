44b4a70f4a1d368f3ed5feef02497b2ccd464f6b
Tyler Brittain <tbrittain100@gmail.com>
2025-09-04 17:11:14 -0700

    add udp server example

---
+++ ch2/UDPServer.py
@@ -0,0 +1,10 @@
+from socket import *
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('',serverPort))
+
+print('the server is ready to recieve')
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
